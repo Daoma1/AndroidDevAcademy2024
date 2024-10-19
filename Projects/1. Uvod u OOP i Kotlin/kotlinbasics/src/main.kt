5aacaa44c9a246147dc6c56f5ff6eda32714e75e
Bruno Zoric <zoric.bruno@gmail.com>
2024-10-20 01:03:43 +0200

    Add presentation for the first lecture. Add project for the first lecture.

---
+++ Projects/1. Uvod u OOP i Kotlin/kotlinbasics/src/main.kt
@@ -0,0 +1,13 @@
+fun main() {
+//    runExample1()
+//    runExample2()
+//    runExample3()
+//    runExample4()
+//    runExample5()
+//    runExample6()
+//    runExample7()
+//    runExample8()
+//    runExample9()
+    runExample10()
+}
+
